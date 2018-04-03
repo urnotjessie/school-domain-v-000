4299d188b4b7609f2e13baab0f50cb98f4ccfec3
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-03 04:18:04 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,9 +2,13 @@
 class School
 
     attr_reader :roster
-    
+
     def initialize
         @roster = {}
     end
+
+    def add_student(name, grade)
+        @roster[grade] = []
+        @roster[grade] << name
 
 end
